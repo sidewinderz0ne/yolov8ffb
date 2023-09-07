481f661fd0d89c676ae5b23124cfcb26be369eb2
sidewinderz0ne <pelangisokoaji@gmail.com>
2023-09-07 09:10:45 +0700

    add pmssql

---
+++ pmssql-quality.py
@@ -0,0 +1,23 @@
+import pymssql
+
+conn = pymssql.connect(
+    server='192.168.1.254\DBSTAGING',
+    user='usertesting',
+    password='Qwerty@123',
+    database='skmstagingdb',
+    as_dict=True
+)
+
+SQL_QUERY = """
+SELECT *
+FROM MOPQuality_Staging;
+"""
+
+cursor = conn.cursor()
+cursor.execute(SQL_QUERY)
+
+records = cursor.fetchall()
+for r in records:
+    print(r)  # This will print all columns for each row
+
+conn.close()  # Close the database connection when done
