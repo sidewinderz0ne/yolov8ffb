0d25f27fcf9a7cb5af6a4a70bb876e0252bd8ea9
Dendy Surya <dendysurya15@gmail.com>
2023-09-14 17:31:27 +0700

    Merge Login Register Frame List Mill

--- app_pos_grading_sampling_yolov8_ppro2.py
+++ app_pos_grading_sampling_yolov8_ppro2.py
@@ -11,6 +11,11 @@ import tkinter.font as tkFont
 import subprocess
 import pymssql
 import re
+from urllib.request import urlopen
+import json
+import hashlib
+
+url = "https://srs-ssms.com/grading_ai/get_list_mill.php"
 
 columns = ('no', 'notiket', 'nopol', 'driver', 'b_unit','divisi', 'field', 'bunches', 'ownership', 'pushtime', 'action')
 
@@ -23,6 +28,14 @@ log_data = Path(os.getcwd() + '/data')
 data_bunit = Path(str(log_data) + '/bunit_mapping.txt')
 data_div = Path(str(log_data) + '/div_mapping.txt')
 data_block = Path(str(log_data) + '/blok_mapping.txt')
+log_mill = Path(str(log_data) + '/mill_mapping.txt')
+
+dir_user = Path(os.getcwd() + '/user')
+dir_user.mkdir(parents=True, exist_ok=True)  
+log_data_user = Path(str(dir_user) + '/data.txt')
+
+if not log_data_user.exists():
+    log_data_user.touch()
 
 log_inference = Path(os.getcwd() + '/hasil')
 
@@ -36,6 +49,221 @@ if not log_dir.exists():
 
 def remove_non_numeric(input_str):
     return re.sub(r'[^0-9.]', '', input_str)
+
+def get_list_mill(dir_mill, flag):
+
+    mill_names = []
+    if not dir_mill.exists():
+        dir_mill.touch()
+
+    with open(dir_mill, 'r') as file:
+        mill_names = [line.strip() for line in file]
+
+    if len(mill_names) == 0:
+        try:
+            response = urlopen(url)
+            get_mill_arr = json.loads(response.read())
+            mill_names = [f"{data['mill']};{data['ip']}" for data in get_mill_arr]
+        except Exception as e:
+            print("An error occurred while fetching the server data:", str(e))
+       
+    
+        with open(dir_mill, 'w') as file:
+            for data in mill_names:
+                file.write(f"{data}\n")
+
+    if flag:
+        mill_names = [f"CCTV {index + 1} ({entry.split(';')[1]})" for index, entry in enumerate(mill_names)]
+        return mill_names
+    
+    mill_names = [entry.split(';')[0] for entry in mill_names]
+
+    #untuk mengambil satu value dari duplicate value txt
+    mill_names = list(set(mill_names))
+    return mill_names
+
+def store_list_mill(dir_mill, mill):
+
+    if not dir_mill.exists():
+        dir_mill.touch()
+
+    selected_mill = mill.split(';')[0]
+
+    with open(dir_mill, 'r') as file:
+        lines = file.readlines()
+
+    filtered_lines = [line for line in lines if line.strip().split(';')[0] == selected_mill]
+
+    with open(dir_mill, 'w') as file:
+        file.writelines(filtered_lines) 
+
+class LoginFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        
+        login_label = tk.Label(self, text="Login", font=("Times New Roman", 20, "bold"))
+        login_label.grid(row=0, column=0, columnspan=2, sticky='w', pady=(40, 0))
+
+        login_label = tk.Label(self, text="Masukkan user dan password untuk mengakses sistem!", font=("Times New Roman", 12, "italic"))
+        login_label.grid(row=1, column=0, columnspan=2, sticky='w', pady=10)
+
+        username_label = tk.Label(self, text="Username:")
+        username_label.grid(row=2, column=0, sticky="w", pady=5)
+        self.username_entry = tk.Entry(self)
+        self.username_entry.grid(row=2, column=1, pady=5, sticky="ew")
+
+        password_label = tk.Label(self, text="Password:")
+        password_label.grid(row=3, column=0, sticky="w")
+        self.password_entry = tk.Entry(self, show="*") 
+        self.password_entry.grid(row=3, column=1, pady=5, sticky="ew")
+
+        # Add a dropdown field
+        mill_label = tk.Label(self, text="Mill:")
+        mill_label.grid(row=4, column=0, sticky="w", pady=5)
+        self.mill_var = tk.StringVar()
+        mill_choices = get_list_mill(log_mill, flag=False)
+        self.mill_combobox = ttk.Combobox(self, textvariable=self.mill_var, values=mill_choices)
+
+        if len(mill_choices) ==1:
+            self.mill_var.set(mill_choices[0])
+        self.mill_combobox.grid(row=4, column=1, pady=5, sticky="ew")
+
+        self.feedback_label = tk.Label(self, text="", fg="red")
+        self.feedback_label.grid(row=6, column=0, columnspan=2, pady=(30, 10))
+
+        register_label = tk.Label(self, text="Belum memiliki akun ?", font=("Times New Roman", 12, "italic"), fg="blue", cursor="hand2")
+        register_label.grid(row=5, column=0, columnspan=2, sticky="e")
+        register_label.bind("<Button-1>", self.register_clicked)
+        
+        login_button = tk.Button(self, text="Login", command=self.login)
+        login_button.grid(row=7, column=0, columnspan=2, sticky="w", pady=(40, 0))
+
+        self.password_entry.bind("<Return>", lambda event: self.login())
+
+    def clear_feedback(self):
+        if self.feedback_label.winfo_exists():
+            self.feedback_label.config(text="")
+
+    def register_clicked(self, event):
+        self.master.switch_frame(RegisterFrame)
+
+    def login(self):
+        user = self.username_entry.get()
+        password = self.password_entry.get()
+        mill = self.mill_combobox.get()
+
+        if not user or not password or not mill:
+            self.feedback_label.config(text="Semua kolom harus diisi", fg="red")
+            self.after(3000, self.clear_feedback)
+            return
+
+        user_exists = False
+        with open(log_data_user, 'r') as log_file:
+            for line in log_file:
+                stored_user, stored_hashed_password = line.strip().split(';')
+                if stored_user == user:
+                    user_exists = True
+
+                    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+                    if hashed_password == stored_hashed_password:
+                        print("Login successful")
+                        store_list_mill(log_mill, mill)
+                        self.master.switch_frame(Frame1)
+                    else:
+                        print("Incorrect password")
+                        self.feedback_label.config(text="Incorrect password", fg="red")
+                    break
+
+        if not user_exists:
+            print("User not found")
+            self.feedback_label.config(text="User not found", fg="red")
+
+        self.after(3000, self.clear_feedback)
+
+
+class RegisterFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        
+        login_label = tk.Label(self, text="Register New User", font=("Times New Roman", 20, "bold"))
+        login_label.grid(row=0, column=0, columnspan=2, sticky='w', pady=(40, 0))
+
+        login_label = tk.Label(self, text="Silakan isi untuk menambahkan user ke sistem!", font=("Times New Roman", 12, "italic"))
+        login_label.grid(row=1, column=0, columnspan=2, sticky='w', pady=10)
+
+        user_root = tk.Label(self, text="User Root:")
+        user_root.grid(row=2, column=0, sticky="w", pady=5)
+        self.useroot_entry = tk.Entry(self)
+        self.useroot_entry.grid(row=2, column=1, pady=5, sticky="ew")
+
+        username_label = tk.Label(self, text="Username:")
+        username_label.grid(row=3, column=0, sticky="w", pady=5)
+        self.username_entry = tk.Entry(self)
+        self.username_entry.grid(row=3, column=1, pady=5, sticky="ew")
+
+        password_label = tk.Label(self, text="Password:")
+        password_label.grid(row=4, column=0, sticky="w", pady=5)
+        self.password_entry = tk.Entry(self, show="*") 
+        self.password_entry.grid(row=4, column=1, sticky="ew")
+        
+        login_button = tk.Button(self, text="Submit", command=self.register)
+        login_button.grid(row=5, column=0,sticky="w",   pady=(40, 0))
+        
+        login_button = tk.Button(self, text="Kembali ke Login", command=self.backToLogin)
+        login_button.grid(row=5, column=1,sticky="w",   pady=(40, 0))
+
+        self.feedback_label = tk.Label(self, text="", fg="green")
+        self.feedback_label.grid(row=6, column=0, columnspan=2, pady=(30, 10))
+
+        self.feedback_label_success = tk.Label(self, text="", fg="red")
+        self.feedback_label_success.grid(row=6, column=0, columnspan=2, pady=(30, 10))
+
+    def backToLogin(self):
+            self.master.switch_frame(LoginFrame)
+
+    def clear_feedback(self):
+        if self.feedback_label.winfo_exists():
+            self.feedback_label.config(text="")
+
+        elif self.feedback_label_success.winfo_exists():
+            self.feedback_label_success.config(text="")
+
+    def register(self):
+            userRoot = self.useroot_entry.get()
+            user = self.username_entry.get()
+            password = self.password_entry.get()
+            
+            if not user or not password or not userRoot:
+                self.feedback_label.config(text="Semua kolom harus diisi", fg="red")
+                self.after(3000, self.clear_feedback)
+                return  # Stop further execution
+
+            if userRoot == 'grading':
+                hashed_password = hashlib.sha256(password.encode()).hexdigest()
+                strUser = user + ';' + hashed_password
+
+                log_file_path = log_data_user
+                user_exists = False
+
+                with open(log_file_path, 'r') as log_file:
+                    for line in log_file:
+                        stored_user, _ = line.strip().split(';')
+                        if stored_user == user:
+                            user_exists = True
+                            break
+
+                if user_exists:
+                    self.feedback_label.config(text="User tersebut sudah terdaftar", fg="red")
+                else:
+                    with open(log_file_path, 'a') as log_file:
+                        log_file.write(strUser + '\n')  # Append the result to the log file with a newline character
+                        print("Data saved successfully to", log_file_path)
+                        self.feedback_label_success.config(text="User baru tersimpan", fg="green")
+            else:    
+                print('Tidak dapat menyimpan User!')
+                self.feedback_label.config(text="Tidak dapat menyimpan User!", fg="red")
+
+            self.after(3000, self.clear_feedback)
 class Frame1(tk.Frame):
 
     def __init__(self, master):
@@ -82,7 +310,7 @@ class Frame1(tk.Frame):
         top_frame.grid_columnconfigure(3, weight=28)
 
 
-        self.title_label = tk.Label(top_frame, text="TABEL LIST TRUK FFB GRADING PER TRUK SCM", font=("Helvetica", 16, "bold"))
+        self.title_label = tk.Label(top_frame, text=f"TABEL LIST TRUK FFB GRADING PER TRUK {get_list_mill(log_mill,flag=False)[0]}", font=("Helvetica", 16, "bold"))
         self.title_label.grid(row=0, column=2)
         self.logo_image = tk.PhotoImage(file=Path(os.getcwd() + '/default-img/Logo-CBI(4).png'))  # Replace "logo.png" with your image file path
         self.logo_image = self.logo_image.subsample(2, 2)  # Adjust the subsample values to resize the image
@@ -94,9 +322,15 @@ class Frame1(tk.Frame):
         logo_label2 = tk.Label(top_frame, image=self.logo_image2)
         logo_label2.grid(row=0, column=1)  # Change column to 0 and sticky to "ne"
 
-         # Create a styled button
+        self.mill_var = tk.StringVar()
+        cctv_choices = get_list_mill(log_mill, flag=True)
+        cctv_choices.append('Video - Test')
+        self.cctv_combobox = ttk.Combobox(self, textvariable=self.mill_var, values=cctv_choices)
+        self.mill_var.set(cctv_choices[0])
+        self.cctv_combobox.grid(row=0, column=3)
+        
         self.button = ttk.Button(top_frame, text="REFRESH", style="Accent.TButton", command=self.refresh_data)
-        self.button.grid(row=0, column=3)
+        self.button.grid(row=0, column=4)
 
         self.refresh_data()
 
@@ -118,6 +352,11 @@ class Frame1(tk.Frame):
         self.columnconfigure(0, weight=1)
 
         self.running_script = False  # Flag to track if script is running
+
+    def on_combobox_focus_out(self, event):
+        selected_value = self.mill_var.get()
+        self.selected_combobox_value = selected_value  # Store the selected value in a class variable
+        print("Selected value:", selected_value)
 
 
     def populate_treeview(self, arrData):
@@ -557,32 +796,58 @@ class Frame3(tk.Frame):
 class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Sistem Aplikasi Pos Grading")  # Set the title here
+        self.title("Sistem Aplikasi Pos Grading")
         
-        # Set the desired width and height for the window
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
-        window_width = int(screen_width * 1)  # 90% of the screen width
-        window_height = int(screen_height * 1)  # 90% of the screen height
-        self.output_inference = None  # Initialize output_inference in MainWindow
-        self.row_values = None  # Initialize row_values in MainWindow
-        
-        self.geometry(f"{window_width}x{window_height}")
-        
-        self.frame1 = Frame1(self)
-        self.frame3 = Frame3(self,  self.output_inference , self.row_values)  
 
-        self.current_frame = self.frame1
-        self.current_frame.pack(padx=50, pady=50)
+        self.login_frame = LoginFrame(self)
+
+        self.current_frame = None  # Initialize current_frame to None
+        # self.current_frame.pack(padx=50, pady=50)
+        self.switch_frame(LoginFrame)  # Start with LoginFrame
+
+        # Set the main window size and position based on the initial frame
+        window_width = 500
+        window_height = 500
+        x = (screen_width - window_width) // 2
+        y = (screen_height - window_height) // 2
+        self.geometry(f"{window_width}x{window_height}+{x}+{y}")
+        
 
     def switch_frame(self, new_frame_class, output_inference=None, row_values=None):
-        self.current_frame.destroy()
+        if self.current_frame is not None:
+            self.current_frame.destroy()
+
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+
         if new_frame_class == Frame3:
-            # Pass output_inference and row_values to Frame3
             self.current_frame = Frame3(self, output_inference, row_values)
+        elif new_frame_class == RegisterFrame:
+            self.current_frame = RegisterFrame(self)
         else:
             self.current_frame = new_frame_class(self)
-        self.current_frame.pack()
+
+        if isinstance(self.current_frame, Frame1):
+            self.current_frame.pack(padx=50, pady=50)  # Add padding for Frame1
+        else:
+            self.current_frame.pack()
+
+        if isinstance(self.current_frame, LoginFrame):
+            window_width = 500
+            window_height = 500
+        elif isinstance(self.current_frame, RegisterFrame):
+            window_width = 500
+            window_height = 500
+        else:
+            window_width = screen_width
+            window_height = screen_height
+
+        x = (screen_width - window_width) // 2
+        y = (screen_height - window_height) // 2
+
+        self.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 if __name__ == "__main__":
     app = MainWindow()
